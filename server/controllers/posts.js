1ab2d3050ae226b04bee9fe26a3790f0a1d05db3
Hadhemi33 <hadhemi.benmansour@isimg.tn>
2023-02-25 00:15:55 +0100

    signin sign up

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,46 +1,61 @@
-import mongoose from 'mongoose';
-import PostMessage from '../Models/postMessage.js'
+import mongoose from "mongoose";
+import PostMessage from "../Models/postMessage.js";
 
-export const getPosts = async(req,res)=>{
-    try{
-        const postMessages = await PostMessage.find();
-        res.send(postMessages) 
-     
-    }
-    catch(error){
-        res.status(404).json({message:error.message});
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    res.send(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+export const createPost = async (req, res) => {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("Not post with this id");
+  const updatePost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  });
+  res.json(updatePost);
+};
 
-    }
-}
-export const createPost = async(req,res)=>{
-    const post = req.body;
-    const newPost = new PostMessage(post);
-    try{
-        await newPost.save();
-        res.status(201).json(newPost);
-    }
-    catch(error){
-        res.status(409).json({message:error.message});
-    }
-}
-export const updatePost = async(req,res)=>{
-    const{id:_id} = req.params;
-    const post = req.body;
-    if(!mongoose.Types.ObjectId.isValid(_id))  return res.status(404).send('Not post with this id');
-    const updatePost = await PostMessage.findByIdAndUpdate(_id,post,{new:true});
-    res.json(updatePost);
-}
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("Not post with this id");
+  const deletePost = await PostMessage.findByIdAndRemove(id);
+  res.json({ message: "post is deleted succesfuly" });
+};
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+  if (!req.userId) return res.json({ message: "Unauthenticated" });
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("Not post with this id");
+  const post = await PostMessage.findById(id);
+  const index = post.likes.findIndex((id) => id === String(req.userId));
+  if (index === -1) {
+    //like
+    post.likes.push(req.userId);
+  } else {
+    //dislike
+    post.likes = post.likes.filter((id) => id !== String(req.userId));
+  }
 
-export const deletePost = async(req,res)=>{
-    const{id}=req.params;
-    if(!mongoose.Types.ObjectId.isValid(id))  return res.status(404).send('Not post with this id');
-    const deletePost = await PostMessage.findByIdAndRemove(id);
-    res.json({message:'post is deleted succesfuly'});
-}
-export const likePost =async (req,res)=>{
-    const{id}=req.params;
-    if(!mongoose.Types.ObjectId.isValid(id))  return res.status(404).send('Not post with this id');
-    const post =  await PostMessage.findById(id);
-    const updatePost = await PostMessage.findByIdAndUpdate(id, {likeCount : post.likeCount +1},{new:true});
-    res.json(updatePost);
-}
+  const updatePost = await PostMessage.findByIdAndUpdate(
+    id,
+    { likeCount: post.likeCount + 1 },
+    { new: true }
+  );
+  res.json(updatePost);
+};
--- server/index.js
+++ server/index.js
@@ -3,14 +3,17 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postsRoutes from "./routes/posts.js";
-import dotenv from "dotenv";
+import userRoutes from "./routes/users.js";
 
+import dotenv from "dotenv";
+/////
 const app = express();
 dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postsRoutes);
+app.use("/user", userRoutes);
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
